aed6ef45957244c8dbc374ae89ea598dd6ede874
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2023-02-06 17:16:22 +0530

    snake movements

--- gameScript.js
+++ gameScript.js
@@ -1,6 +1,7 @@
 var lastPaintTime = 0;
 const SNAKE_SPEED = 1;
 let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = inputDirection;
 
 const snakeBody = [
   { x: 8, y: 8 },
@@ -43,9 +44,28 @@ function drawSnake() {
     var snakeElement = document.createElement("div");
     snakeElement.style.gridColumnStart = segment.x;
     snakeElement.style.gridRowStart = segment.y;
+    // snakeElement.innerHTML = index;
+    snakeElement.style.transform = "rotate(0deg)";
 
-    if (index === 0) {
+    if (index == 0) {
       snakeElement.classList.add("head");
+
+      // If x = 1, snake is going towards right
+      if (inputDirection.x == 1) {
+        snakeElement.style.transform = "rotate(-90deg)";
+      }
+      // If x = -1, snake is going towards left
+      else if (inputDirection.x == -1) {
+        snakeElement.style.transform = "rotate(90deg)";
+      }
+      // If y = -1, snake is going upwards
+      else if (inputDirection.y == -1) {
+        snakeElement.style.transform = "rotate(180deg)";
+      }
+      // If y = 1, snake is going downwards
+      else if (inputDirection.y == 1) {
+        snakeElement.style.transform = "rotate(0deg)";
+      }
     } else {
       snakeElement.classList.add("snake");
     }
@@ -60,7 +80,7 @@ function snakeMove() {
   // make not just head, but other segments too move with head -- select last element and shift it to second last position and so on
 
   for (i = snakeBody.length - 2; i >= 0; i--) {
-    snakeBody[i + 1] = { ...snakeBody };
+    snakeBody[i + 1] = { ...snakeBody[i] };
   }
 
   //snake moving forward towards right
@@ -74,20 +94,25 @@ function getInputDirection() {
 
     switch (e.key) {
       case "ArrowUp":
+        if (lastInputDirection.y == 1) break;
         inputDirection = { x: 0, y: -1 };
         break;
       case "ArrowDown":
+        if (lastInputDirection.y == -1) break;
         inputDirection = { x: 0, y: 1 };
         break;
       case "ArrowLeft":
+        if (lastInputDirection.x == 1) break;
         inputDirection = { x: -1, y: 0 };
         break;
       case "ArrowRight":
+        if (lastInputDirection.x == -1) break;
         inputDirection = { x: 1, y: 0 };
         break;
       default:
         inputDirection = { x: 0, y: 0 };
     }
   });
+  lastInputDirection = inputDirection;
   return inputDirection;
 }
